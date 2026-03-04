8b6f7515998b016a4a9ade39011d51f178348963
islam0122 <duishobaevislam01@gmail.com>
2026-03-04 18:47:22 +0600

    saddsa

---
+++ app.py
@@ -0,0 +1,16 @@
+from dataclasses import asdict
+from gc import disable
+from pstats import add_func_stats
+
+dasdsa
+disable(asdict(sadsda
+
+               add_func_stats(
+                   sda
+                   ads
+
+
+                   disable()
+                   add_func_stats
+                   dsa
+               )))
