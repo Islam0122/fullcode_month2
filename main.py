8ffd711191941ad7431c072cea41ac4ebdf36034
islam0122 <duishobaevislam01@gmail.com>
2026-03-05 18:16:54 +0600

    create command start

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("Hello World")
-sadhhjhj
+import asyncio
+import os
+from dotenv import load_dotenv
+from  aiogram import Bot,Dispatcher
+from aiogram.types import Message,FSInputFile
+from aiogram.filters import Command
+
+load_dotenv()
+
+Token = os.getenv("Token")
+
+bot = Bot(token=Token)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def start_command(message: Message):
+    await message.answer_photo(
+        photo=FSInputFile("./img.png"),
+        caption="👋 Добро пожаловать в Finance Bot!\n\n"
+        "Этот бот поможет учитывать доходы и расходы.",
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
