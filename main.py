364d6ac99194450b34fe797a055dd7e13beafebf
Dastanbek <dastanbekuushka@gmail.com>
2026-03-05 19:15:55 +0600

    bot of dastan

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ async def start_command(message: Message):
         "Этот бот поможет учитывать доходы и расходы.",
     )
 
+@dp.message(Command('aboutus'))
+async def start_command(message: Message):
+    await message.answer_photo(
+        photo=FSInputFile("./img.png"),
+        caption="👋 Добро пожаловать меня зовут Finance Bot!\n\n"
+                "Я помогу тебе учитывать доходы и расходы.",
+    )
+@dp.message(Command('help'))
+async def start_command(message: Message):
+    await message.answer_photo(
+        photo=FSInputFile("./img.png"),
+        caption="👋 Чем тебе помочь?"
+    )
+
 async def main():
     await dp.start_polling(bot)
 
